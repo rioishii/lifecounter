4b6272ddd4293c65c6dca436205bc8e1d090630f
rioishii <rioishii@uw.edu>
2019-01-27 10:56:16 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Rio Ishii on 1/27/19.
+//  Copyright © 2019 Rio Ishii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
